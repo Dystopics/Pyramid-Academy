429c38b3a3ed32160e3ef1f494a463db82b2cb13
J.Schmitz <apocatech@gmail.com>
2022-02-08 12:01:51 -0500

    Week 1 Projects
    Dragon-cave and Guess-the-Number projects

---
+++ Guess-The-Number/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner input = new Scanner(System.in);
+        Random generator = new Random();
+        String name = new String();
+
+        int secretNum = generator.nextInt(20) + 1;
+        int uGuess;
+        int guessNum = 0;
+        String playAgain = "y";
+
+        //prompt for name
+        System.out.println("Hello! What is your name?");
+
+        name = input.nextLine();
+
+        //do while loop containing input
+        //repeat until correct guess
+        do{
+        //continued prompt
+            System.out.println("Well, " + name + ", I am thinking of a number between 1 and 20.");
+            do{
+                System.out.println("Take a guess.");
+                uGuess = input.nextInt();
+
+                guessNum++;
+
+                if(uGuess > secretNum)
+                    System.out.println("Your guess is too high.");
+                else if(uGuess < secretNum)
+                    System.out.println("Your guess is too low.");
+                else {
+                    System.out.println("Good job, " + name + "! You guess my number in " + guessNum + " guesses!");
+                    System.out.println("Would you like to play again? (y or n)");
+                    playAgain = input.next();
+                }
+            }while(secretNum != uGuess);
+        }while(playAgain.equals("y"));
+
+    }
+}
